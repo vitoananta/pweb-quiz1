3aabad610a9538127fa8eb772bbab1b3c47de6e4
Vito Febrian Ananta <vitoananta3@gmail.com>
2023-09-28 09:08:08 +0700

    Food page base

--- quiz1/js/index.js
+++ quiz1/js/index.js
@@ -12,5 +12,58 @@ document.addEventListener('click', (e) => {
     if (!hamburger.contains(e.target) && !mainNav.contains(e.target)) {
         mainNav.classList.remove('active');
     }
+});
 
+const foods = document.querySelectorAll('.foods');
+const sotoBetawi = document.querySelector('.soto-betawi-title');
+const kerakTelor = document.querySelector('.kerak-telor-title');
+const nasiUduk = document.querySelector('.nasi-uduk-title');
+const gadoGado = document.querySelector('.gado-gado-title');
+
+window.addEventListener('scroll', function () {
+    let currentfood = '';
+
+    foods.forEach(food => {
+        const foodTop = food.offsetTop;
+        const foodHeight = food.clientHeight;
+
+        if (pageYOffset >= (foodTop - foodHeight / 3)) {
+            currentfood = food.getAttribute('id');
+        }
+    });
+
+    if (currentfood && pageYOffset + window.innerHeight < document.body.clientHeight) {
+        switch (currentfood) {
+            case 'soto-betawi':
+                sotoBetawi.classList.add('show');
+                kerakTelor.classList.remove('show');
+                nasiUduk.classList.remove('show');
+                gadoGado.classList.remove('show');
+                break;
+            case 'kerak-telor':
+                kerakTelor.classList.add('show');
+                sotoBetawi.classList.remove('show');
+                nasiUduk.classList.remove('show');
+                gadoGado.classList.remove('show');
+                break;
+            case 'nasi-uduk':
+                nasiUduk.classList.add('show');
+                sotoBetawi.classList.remove('show');
+                kerakTelor.classList.remove('show');
+                gadoGado.classList.remove('show');
+                break;
+            case 'gado-gado':
+                gadoGado.classList.add('show');
+                sotoBetawi.classList.remove('show');
+                kerakTelor.classList.remove('show');
+                nasiUduk.classList.remove('show');
+                break;
+            default:
+                sotoBetawi.classList.add('show');
+                kerakTelor.classList.remove('show');
+                nasiUduk.classList.remove('show');
+                gadoGado.classList.remove('show');
+                break;
+        }
+    }
 });
