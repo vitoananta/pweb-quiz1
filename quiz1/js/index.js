4992b0f9d8aed2f097d438f31c540eb0173478ce
Vito Febrian Ananta <vitoananta3@gmail.com>
2023-09-28 17:25:20 +0700

    Responsive food page

--- quiz1/js/index.js
+++ quiz1/js/index.js
@@ -2,12 +2,12 @@
 const mainNav = document.querySelector('.main-nav');
 
 // When click on hamburger menu
-document.querySelector('#hamburger-menu').onclick = () => {
+const hamburger = document.querySelector('#hamburger-menu');
+hamburger.onclick = () => {
     mainNav.classList.toggle('active');
 };
 
 // Close hamburger menu when click outside of it
-const hamburger = document.querySelector('#hamburger-menu');
 document.addEventListener('click', (e) => {
     if (!hamburger.contains(e.target) && !mainNav.contains(e.target)) {
         mainNav.classList.remove('active');
@@ -44,50 +44,75 @@ window.addEventListener('scroll', function () {
                 kerakTelor.classList.remove('show');
                 nasiUduk.classList.remove('show');
                 gadoGado.classList.remove('show');
+
                 sotoBetawiButton.classList.add('show');
+                sotoBetawiButton.style.display = 'flex';
                 kerakTelorButton.classList.remove('show');
+                kerakTelorButton.style.display = 'none';
                 nasiUdukButton.classList.remove('show');
+                nasiUdukButton.style.display = 'none';
                 gadoGadoButton.classList.remove('show');
+                gadoGadoButton.style.display = 'none';
                 break;
             case 'kerak-telor':
                 kerakTelor.classList.add('show');
                 sotoBetawi.classList.remove('show');
                 nasiUduk.classList.remove('show');
                 gadoGado.classList.remove('show');
+
                 kerakTelorButton.classList.add('show');
+                kerakTelorButton.style.display = 'flex';
                 sotoBetawiButton.classList.remove('show');
+                sotoBetawiButton.style.display = 'none';
                 nasiUdukButton.classList.remove('show');
+                nasiUdukButton.style.display = 'none';
                 gadoGadoButton.classList.remove('show');
+                gadoGadoButton.style.display = 'none';
                 break;
             case 'nasi-uduk':
                 nasiUduk.classList.add('show');
                 sotoBetawi.classList.remove('show');
                 kerakTelor.classList.remove('show');
                 gadoGado.classList.remove('show');
+
                 nasiUdukButton.classList.add('show');
+                nasiUdukButton.style.display = 'flex';
                 sotoBetawiButton.classList.remove('show');
+                sotoBetawiButton.style.display = 'none';
                 kerakTelorButton.classList.remove('show');
+                kerakTelorButton.style.display = 'none';
                 gadoGadoButton.classList.remove('show');
+                gadoGadoButton.style.display = 'none';
                 break;
             case 'gado-gado':
                 gadoGado.classList.add('show');
                 sotoBetawi.classList.remove('show');
                 kerakTelor.classList.remove('show');
                 nasiUduk.classList.remove('show');
+
                 gadoGadoButton.classList.add('show');
+                gadoGadoButton.style.display = 'flex';
                 sotoBetawiButton.classList.remove('show');
+                sotoBetawiButton.style.display = 'none';
                 kerakTelorButton.classList.remove('show');
+                kerakTelorButton.style.display = 'none';
                 nasiUdukButton.classList.remove('show');
+                nasiUdukButton.style.display = 'none';
                 break;
             default:
                 sotoBetawi.classList.add('show');
                 kerakTelor.classList.remove('show');
                 nasiUduk.classList.remove('show');
                 gadoGado.classList.remove('show');
+
                 sotoBetawiButton.classList.add('show');
+                sotoBetawiButton.style.display = 'flex';
                 kerakTelorButton.classList.remove('show');
+                kerakTelorButton.style.display = 'none';
                 nasiUdukButton.classList.remove('show');
+                nasiUdukButton.style.display = 'none';
                 gadoGadoButton.classList.remove('show');
+                gadoGadoButton.style.display = 'none';
                 break;
         }
     }
@@ -95,21 +120,92 @@ window.addEventListener('scroll', function () {
 
 // Soto betawi modal box
 const sotoBetawiModal = document.querySelector('.soto-betawi-modal');
+const sotoBetawiModalBox = document.querySelector('.soto-betawi-box');
 const sotoBetawiModalButton = document.querySelector('.soto-betawi-modal-button');
+const sotoBetawiCloseIcon = document.querySelector('.soto-betawi-close-icon');
 
-sotoBetawiModalButton.onclick = () => {
+sotoBetawiModalButton.onclick = (e) => {
     sotoBetawiModal.style.display = 'flex';
-    console.log('clicked open');
+    e.preventDefault();
 }
 
-document.querySelector('.close-icon').onclick = (e) => {
+sotoBetawiCloseIcon.onclick = (e) => {
     sotoBetawiModal.style.display = 'none';
-    console.log('clicked close icon');
+    e.preventDefault();
 }
 
-window.onclick = (e) => {
-    if (e.target === sotoBetawiModal) {
+document.addEventListener('click', (e) => {
+    if (!sotoBetawiModalBox.contains(e.target) && !mainNav.contains(e.target) && !sotoBetawiModalButton.contains(e.target)) {
         sotoBetawiModal.style.display = 'none';
-        console.log('clicked close outisde');
+        e.preventDefault();
     }
+});
+
+// Kerak telor modal box
+const kerakTelorModal = document.querySelector('.kerak-telor-modal');
+const kerakTelorModalBox = document.querySelector('.kerak-telor-box');
+const kerakTelorModalButton = document.querySelector('.kerak-telor-modal-button');
+const kerakTelorCloseIcon = document.querySelector('.kerak-telor-close-icon');
+
+kerakTelorModalButton.onclick = (e) => {
+    kerakTelorModal.style.display = 'flex';
+    e.preventDefault();
 }
+
+kerakTelorCloseIcon.onclick = (e) => {
+    kerakTelorModal.style.display = 'none';
+    e.preventDefault();
+}
+
+document.addEventListener('click', (e) => {
+    if (!kerakTelorModalBox.contains(e.target) && !mainNav.contains(e.target) && !kerakTelorModalButton.contains(e.target)) {
+        kerakTelorModal.style.display = 'none';
+        e.preventDefault();
+    }
+});
+
+// Nasi Uduk modal box
+const nasiUdukModal = document.querySelector('.nasi-uduk-modal');
+const nasiUdukModalBox = document.querySelector('.nasi-uduk-box');
+const nasiUdukModalButton = document.querySelector('.nasi-uduk-modal-button');
+const nasiUdukCloseIcon = document.querySelector('.nasi-uduk-close-icon');
+
+nasiUdukModalButton.onclick = (e) => {
+    nasiUdukModal.style.display = 'flex';
+    e.preventDefault();
+}
+
+nasiUdukCloseIcon.onclick = (e) => {
+    nasiUdukModal.style.display = 'none';
+    e.preventDefault();
+}
+
+document.addEventListener('click', (e) => {
+    if (!nasiUdukModalBox.contains(e.target) && !mainNav.contains(e.target) && !nasiUdukModalButton.contains(e.target)) {
+        nasiUdukModal.style.display = 'none';
+        e.preventDefault();
+    }
+});
+
+// Gado-Gado modal box
+const gadoGadoModal = document.querySelector('.gado-gado-modal');
+const gadoGadoModalBox = document.querySelector('.gado-gado-box');
+const gadoGadoModalButton = document.querySelector('.gado-gado-modal-button');
+const gadoGadoCloseIcon = document.querySelector('.gado-gado-close-icon');
+
+gadoGadoModalButton.onclick = (e) => {
+    gadoGadoModal.style.display = 'flex';
+    e.preventDefault();
+}
+
+gadoGadoCloseIcon.onclick = (e) => {
+    gadoGadoModal.style.display = 'none';
+    e.preventDefault();
+}
+
+document.addEventListener('click', (e) => {
+    if (!gadoGadoModalBox.contains(e.target) && !mainNav.contains(e.target) && !gadoGadoModalButton.contains(e.target)) {
+        gadoGadoModal.style.display = 'none';
+        e.preventDefault();
+    }
+});
